642a22b7fbc33fcd86e3303670b90ec9f29bbc58
m999ikl <issam-klai@m-team.be>
2020-02-07 15:31:38 +0100

    ssd

--- storbel_labs/bash_storbel/assets/kubernetes.tf
+++ storbel_labs/bash_storbel/assets/kubernetes.tf
@@ -11,3 +11,36 @@ resource "kubernetes_namespace" "example" {
     name = "terraform-example-namespace"
   }
 }
+
+resource "kubernetes_persistent_volume" "pv-mypv" {
+  metadata {
+    name = "pv-mypv"
+  }
+  spec {
+    capacity = {
+      storage = "2Gi"
+    }
+    access_modes = ["ReadWriteMany","ReadWriteOnce"]
+    persistent_volume_source {
+      vsphere_volume {
+        volume_path = "/data/pv-mypv"
+      }
+    }
+  }
+}
+
+resource "kubernetes_secret" "example" {
+
+  for_each = toset(var.secrets)
+  metadata {
+    name = each.value
+    namespace  = "acs"
+  }
+
+  data = {
+    username = "admin"
+    password = "P4ssw0rd"
+  }
+
+  type = "kubernetes.io/basic-auth"
+}
