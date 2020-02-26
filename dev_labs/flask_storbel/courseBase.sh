1bb91f7079221b64666d5164f936265e2ba561ae
m999ikl <issam-klai@m-team.be>
2020-02-26 15:06:42 +0100

    sd

--- dev_labs/flask_storbel/courseBase.sh
+++ dev_labs/flask_storbel/courseBase.sh
@@ -1,9 +1,14 @@
 
 
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
 
+pip install flask
+git clone https://github.com/storbel/flask.git
 
  wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
  yum install unzip
  unzip terraform_0.12.20_linux_amd64.zip
+ rm -rf terraform_0.12.20_linux_amd64.zip
  mv terraform /usr/local/sbin/
  terraform version
