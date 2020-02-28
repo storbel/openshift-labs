b28578a95dc5b7dd014cbe07ac137df3b772dfcf
m999ikl <issam-klai@m-team.be>
2020-02-28 09:13:10 +0100

    eee

--- dev_labs/flask_storbel/courseBase.sh
+++ dev_labs/flask_storbel/courseBase.sh
@@ -1,18 +1,23 @@
-
-
-wget https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
-
-pip install flask
-git clone https://github.com/storbel/flask.git
-
-git clone https://github.com/miguelgrinberg/flasky.git
-cd flasky
-git checkout 1a
-
+# Install Terraform
  wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
  yum install unzip
  unzip terraform_0.12.20_linux_amd64.zip
  rm -rf terraform_0.12.20_linux_amd64.zip
  mv terraform /usr/local/sbin/
  terraform version
+
+# Install pip
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+# Install Flask
+pip install flask
+
+# Clone Storbel Flask 
+git clone https://github.com/storbel/flask.git
+
+# Clone Flasky
+git clone https://github.com/miguelgrinberg/flasky.git
+cd flasky
+git checkout 1a
+
+
