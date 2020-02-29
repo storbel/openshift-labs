5bb4f6853f62659ff521a45175138d505d1cac07
storbel <contact@storbel.com>
2020-02-29 06:47:38 +0100

    Update courseBase.sh

--- dev_labs/flask_storbel/courseBase.sh
+++ dev_labs/flask_storbel/courseBase.sh
@@ -3,6 +3,7 @@ apt-get update
 apt-get -y install wget  
 apt-get -y install ansible
 apt-get -y install unzip
+apt-get -y install sqlite
  wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
  unzip terraform_0.12.20_linux_amd64.zip
  rm -rf terraform_0.12.20_linux_amd64.zip
